cadbc08a20dcaebff91490c8129e5bc6ba61efbe
SoumikMahajan <soumik800@gmail.com>
2022-10-22 14:58:57 +0530

    Accoutant new module

---
+++ ASBM/Models/AccountantModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASBM.Models
+{
+    public class AccountantModel
+    {
+        public string voterNo { get; set; }
+        public DateTime voterDate { get; set; }
+    }
+}
---
+++ ASBM/Repository/AccountantClass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASBM.Repository
+{
+    public class AccountantClass
+    {
+    }
+}
